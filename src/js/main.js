ad5c91577459162d33485e4062d9a59ba0d9b82d
natlopar <natalialopezariel@gmail.com>
2024-01-09 10:19:14 +0100

    done

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,39 @@ const msg= document.querySelector('.js-msg');
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
+
+}
+let resultComputer='';
+const computerSelect = () => {
+    const random = getRandomNumber(9);
+    console.log(random);
+    if (random <= 3) {
+        resultComputer = 'piedra';
+    } else if (random >=7) {
+        resultComputer = 'papel';
+    } else {
+        resultComputer ='tijera';
+    } 
+    console.log(resultComputer);
 }
 
 function handleClick(event){
     const playerSelect = userOptions.value;
     console.log(playerSelect);
-
+    getRandomNumber(9);
+    computerSelect();
+    console.log(resultComputer);
+    if (playerSelect === resultComputer) {
+        msg.innerHTML= 'Empate';
+    } else if (playerSelect === 'piedra' && resultComputer === 'tijera') {
+        msg.innerHTML= '¡Has ganado!';
+    } else if (playerSelect === 'papel' && resultComputer === 'piedra'){
+        msg.innerHTML= '¡Has ganado!';
+    } else if (playerSelect === 'tijera' && resultComputer === 'papel'){
+    msg.innerHTML= '¡Has ganado!';
+    } else {
+        msg.innerHTML= '¡Has perdido!';
+    }   
 }
 
 submit.addEventListener('click', handleClick);
