abc8349af8f19ea0172002bd134deb148cb015a7
natlopar <natalialopezariel@gmail.com>
2024-01-09 09:36:02 +0100

    js select

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
+const userOptions= document.querySelector('.js-options');
+const submit= document.querySelector('.js-submit');
+const msg= document.querySelector('.js-msg');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+function handleClick(event){
+    const playerSelect = userOptions.value;
+    console.log(playerSelect);
+
+}
+
+submit.addEventListener('click', handleClick);
+
 
