4b77419122bae00ab5464d9fbf764d4c81248235
natlopar <natalialopezariel@gmail.com>
2024-01-09 11:06:24 +0100

    finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,73 @@
 'use strict';
+ //me ha faltado acumular los puntos de las rondas
 
-const userOptions= document.querySelector('.js-options');
-const submit= document.querySelector('.js-submit');
-const msg= document.querySelector('.js-msg');
+
+const userOptions = document.querySelector('.js-options');
+const submit = document.querySelector('.js-submit');
+const msg = document.querySelector('.js-msg');
+const player = document.querySelector('.js-playerCount');
+const computer = document.querySelector('.js-computerCount');
+
+
 
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-
+  return Math.ceil(Math.random() * max);
 }
-let resultComputer='';
+let selectComputer = '';
 const computerSelect = () => {
-    const random = getRandomNumber(9);
-    console.log(random);
-    if (random <= 3) {
-        resultComputer = 'piedra';
-    } else if (random >=7) {
-        resultComputer = 'papel';
-    } else {
-        resultComputer ='tijera';
-    } 
-    console.log(resultComputer);
-}
+  const random = getRandomNumber(9);
+  console.log(random);
+  if (random <= 3) {
+    selectComputer = 'piedra';
+  } else if (random >= 7) {
+    selectComputer = 'papel';
+  } else {
+    selectComputer = 'tijera';
+  }
+  console.log(selectComputer);
+};
 
-function handleClick(event){
-    const playerSelect = userOptions.value;
-    console.log(playerSelect);
-    getRandomNumber(9);
-    computerSelect();
-    console.log(resultComputer);
-    if (playerSelect === resultComputer) {
-        msg.innerHTML= 'Empate';
-    } else if (playerSelect === 'piedra' && resultComputer === 'tijera') {
-        msg.innerHTML= '¡Has ganado!';
-    } else if (playerSelect === 'papel' && resultComputer === 'piedra'){
-        msg.innerHTML= '¡Has ganado!';
-    } else if (playerSelect === 'tijera' && resultComputer === 'papel'){
-    msg.innerHTML= '¡Has ganado!';
+// let resultComputer = 0;
+// let resultPlayer = 0;
+
+
+function handleClick(event) {
+// for (let i = 0; i < 10; i++) {
+  const playerSelect = userOptions.value;
+  console.log(playerSelect);
+  getRandomNumber(9);
+  computerSelect();
+  console.log(selectComputer);
+  
+    if (playerSelect === selectComputer) {
+      msg.innerHTML = 'Empate';
+    //   resultComputer += 0;
+    //   resultPlayer += 0;
+    //   player.innerHTML= `Jugador: ${resultPlayer}`;
+    //   computer.innerHTML= `Computadora: ${resultComputer}`;
+    } else if (playerSelect === 'piedra' && selectComputer === 'tijera') {
+      msg.innerHTML = '¡Has ganado!';
+    //   resultPlayer += 1;
+    //   resultComputer += 0;
+    //   player.innerHTML= `Jugador: ${resultPlayer}`;
+    //   computer.innerHTML= `Computadora: ${resultComputer}`;
+    } else if (playerSelect === 'papel' && selectComputer === 'piedra') {
+      msg.innerHTML = '¡Has ganado!';
+    //   resultPlayer += 1;
+    //   player.innerHTML= `Jugador: ${resultPlayer}`;
+    //   computer.innerHTML= `Computadora: ${resultComputer}`;
+    } else if (playerSelect === 'tijera' && selectComputer === 'papel') {
+      msg.innerHTML = '¡Has ganado!';
+    //   resultPlayer += 1;
+    //   player.innerHTML= `Jugador: ${resultPlayer}`;
+    //   computer.innerHTML= `Computadora: ${resultComputer}`;
     } else {
-        msg.innerHTML= '¡Has perdido!';
-    }   
+      msg.innerHTML = '¡Has perdido!';
+    //   resultComputer += 1;
+    //   resultPlayer += 0;
+    //   player.innerHTML= `Jugador: ${resultPlayer}`;
+    //   computer.innerHTML= `Computadora: ${resultComputer}`;
+    }
 }
 
 submit.addEventListener('click', handleClick);
-
-
